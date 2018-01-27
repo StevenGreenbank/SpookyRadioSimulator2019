97771ee9574db8ff30cb7f3205093d3d2e14e772
Unknown <steven.greenbank9@gmail.com>
2018-01-27 11:17:51 -0600

    FUCKIN' ASSETS INNIT

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public Camera[] Cams;
+
+	public Camera ControlRoomCamera;
+	public Camera NorthHallwayCamera;
+	public Camera SouthHallwayCamera;
+
+	public Button ControlRoomButton;
+	public Button NorthHallwayButton;
+	public Button SouthHallwayButton;
+
+	AudioSource staticSound;
+
+	// Use this for initialization
+	void Start () {
+		staticSound = FindObjectOfType<AudioSource> ();
+	}
+	
+	public void OnCamButtonClick(string whichButton)
+	{
+		if(whichButton != "ControlRoomButton")
+			staticSound.Play ();
+
+		foreach (Camera cam in Cams)
+			cam.depth = -10;
+
+		if (whichButton == "ControlRoomButton")
+			ControlRoomCamera.depth = 10;
+		else if (whichButton == "NorthHallwayButton")
+			NorthHallwayCamera.depth = 10;
+		else if (whichButton == "SouthHallwayButton")
+			SouthHallwayCamera.depth = 10;
+
+	}
+}
---
+++ Assets/Scripts/CameraScriptDONE.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraScriptDONE : MonoBehaviour {
+
+	public Camera[] Cams;
+	public Camera northHallwayCam;
+	public Camera southHallwayCam;
+	public Camera controlRoomCam;
+
+	public Button northHallwayButton;
+	public Button southHallwayButton;
+	public Button controlRoomButton;
+
+	AudioSource staticSound;
+	// Use this for initialization
+	void Start () {
+		staticSound = FindObjectOfType<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+	
+
+	public void OnCamButtonClicks (string whichButton) {
+		if(whichButton != "controlRoomButton")
+			staticSound.Play ();
+	
+		foreach (Camera cam in Cams)
+		{
+			cam.depth = -10;
+		}
+		if (whichButton == "northHallwayButton") 
+		{
+			northHallwayCam.depth = 1;
+		}
+		else if (whichButton == "southHallwayButton") 
+		{
+			southHallwayCam.depth = 1;
+		}
+		else if (whichButton == "controlRoomButton") 
+		{
+			controlRoomCam.depth = 1;
+		}
+	}
+}
