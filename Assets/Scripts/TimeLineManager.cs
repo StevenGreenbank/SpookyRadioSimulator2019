4f30d5c5cff9d84c4e13ab2c3bcd2935b33ef969
Brenna Curran <brecur@gmail.com>
2018-01-27 19:05:46 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/TimeLineManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour {
+
+    // slider listeners ### set type, init, etc
+    TimelineListener TLQListener;
+   
+
+    // timeline queues
+    TimelineQueue TLQ;
+    int currentSection;
+
+    // Use this for initialization
+    public void Init()
+    {
+        currentSection = 0;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //get timelime cues from listeners
+
+
+        // compare times to timelines, update if nessecary
+
+        // if volumes don't match
+        if (!CompareVolume(TLQ, TLQListener))
+        {
+            StopAndPunish();
+        }
+
+
+        else {
+            // else, adjust ratings and oxygen as normal
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public void ClearAllTimelineQueues()
+    {
+        TLQ.Clear();
+ 
+    }
+
+    private bool AfterEnd()
+    {
+        return TLQListener.getTime() >= TLQ.Peek().timeEnd;
+    }    
+
+    private bool CompareVolume()
+    {
+        return TLQListener.getVolume() == TLQ.Peek().volume; 
+    }
+
+
+    private void StopAndPunish()
+    {
+        // stop clips
+
+
+        // punish player
+
+
+        // move current section to next section
+        currentSection++;
+        TLQ.NextSection();
+
+
+        // clear timelines
+        ClearAllTimelineQueues();
+
+        //and fill them with the next section's timelines
+        FillQueue();
+
+
+    }
+
+    //init and upadte section before using
+    private void FillQueue()
+    {
+        // need a var to store the clip lengths, 
+
+
+        for (int i = 0; i < soundCues.sectionLength(currentSection); i++)
+        {
+            TLQ.Enqueue(soundCues.getCue(currentSection, i));
+
+        }
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/TimelineCue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineCue : MonoBehaviour {
+
+    public Time timeStart;
+    public Time timeEnd;
+    public float volume;
+
+}
