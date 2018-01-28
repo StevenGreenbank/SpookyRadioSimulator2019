c443056a65c34b48f1749589ef097c3c778b80de
Unknown <steven.greenbank9@gmail.com>
2018-01-28 04:38:02 -0600

    Bullshitted event listeners like a fucking baws

---
+++ Assets/MainScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class MainScript : MonoBehaviour {
+    bool CutCallPressed = false;
+    bool PlayAdPressed = false;
+    float slideValue = 80f;
+	// Use this for initialization
+	void Start () {
+		
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("" + CutCallPressed + " " + PlayAdPressed + 
+            " " + slideValue);
+        if (CutCallPressed) {
+            CutCallPressed = false;
+        }
+        if (PlayAdPressed) {
+            PlayAdPressed = false;
+        }
+
+    }
+
+    public void slideUpdate(float value) {
+        slideValue = value;
+    }
+
+    public void CutCall()
+    {
+        CutCallPressed = true; 
+    }
+    public void PlayAd()
+    {
+        PlayAdPressed = true; 
+    }
+}
