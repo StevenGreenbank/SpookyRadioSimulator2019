2c39df81df17bbb33c68abeafabbc51b3f93cf39
Unknown <cornergas14@yahoo.ca>
2018-01-27 19:02:55 -0600

    Chris updates

---
+++ Assets/Scripts/OnOff2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OnOff2 : MonoBehaviour {
+
+    public Text textshow = null;
+    public void changeWord (string word)
+    {
+        if (textshow.text == word)
+            textshow.text = "OFF";
+        else
+            textshow.text = word;
+    }
+}
