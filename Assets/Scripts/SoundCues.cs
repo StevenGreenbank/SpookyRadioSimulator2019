11f73132b90c9281d2a4c7cc4c46e91a1f7b797f
Brenna Curran <brecur@gmail.com>
2018-01-27 19:40:50 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/SoundCues.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCues : MonoBehaviour {
+
+    const int MAX_SECTIONS = 3;
+    const int MAX_CLIPS = 3;
+
+    int[] sectionLength = new int[MAX_SECTIONS];
+    TimelineCue[,] cueList = new TimelineCue[MAX_SECTIONS, MAX_CLIPS];
+    string[] soundClipName = new string[MAX_SECTIONS];
+
+    public int SectionLength(int currentSection)
+    {
+        return sectionLength[currentSection];
+    }
+    
+        
+   public TimelineCue GetCue(int currentSection, int cueIndex)
+   {
+        return cueList[currentSection, cueIndex];
+   }
+}
