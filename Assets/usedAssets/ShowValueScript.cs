09ed45b08d56714a2d1e73fa396d5bcd54a6426d
Unknown <steven.greenbank9@gmail.com>
2018-01-30 20:20:44 -0600

    Trimming the fat

---
+++ Assets/usedAssets/ShowValueScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValueScript : MonoBehaviour {
+
+    Text percentageText;
+    int percentage;
+
+
+	void Start () {
+        percentageText = GetComponent<Text> ();
+	}
+	
+
+	public void textUpdate (float value) {
+
+        percentage = Mathf.RoundToInt(value * 100);
+        percentageText.text = percentage + "%";
+	}
+}
